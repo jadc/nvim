97f041a2e15f1025ec61a3bc9168133ed6c63c96
jadc <chehimj@gmail.com>
2026-07-13 18:09:14 -0400

    Remove mini-files.nvim

--- lua/plugins/mini-files.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({ "https://github.com/echasnovski/mini.files" })
-
-require("mini.files").setup()
-
-vim.keymap.set("n", "-", function()
-    local buf_name = vim.api.nvim_buf_get_name(0)
-    local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
-    require("mini.files").open(path)
-end, { desc = "Open file explorer" })
