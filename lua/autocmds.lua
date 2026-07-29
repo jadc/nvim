56c50acb9666c09356904aa7d97d1804f5f2a44f
jadc <chehimj@gmail.com>
2026-07-28 21:57:49 -0400

    Replace deprecated vim.highlight

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,9 +4,8 @@ local augroup = vim.api.nvim_create_augroup
 -- Highlight yanked text
 local highlight_group = augroup("YankHighlight", { clear = true })
 autocmd("TextYankPost", {
-    pattern = "*",
     callback = function()
-        vim.highlight.on_yank({ timeout = 170 })
+        vim.hl.on_yank({ timeout = 170 })
     end,
     group = highlight_group,
 })
