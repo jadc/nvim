0187380797ff3ca0839e6e0a1704287fce900c9c
jadc <chehimj@gmail.com>
2026-07-28 21:00:06 -0400

    Hoist unpack into local variable

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,8 +9,9 @@ vim.pack.add({
     "https://github.com/saadparwaiz1/cmp_luasnip",
 })
 
+local unpack = unpack or table.unpack
+
 local has_words_before = function()
-    unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
